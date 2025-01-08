6e902f31cc9f84b96cbd70d90adf171de472f86f
andre <151407275+trueandre091@users.noreply.github.com>
2025-01-08 18:23:04 +0200

    800004 800009 800013 800024 800033

--- 08-01-25/Task-25-17879.py
+++ 08-01-25/Task-25-17879.py
@@ -1,17 +1,17 @@
-from sympy import factorint
+from sympy import factorint, divisors
 
 c = 0
-for i in range(800000, 10**7):
+for i in range(800001, 10**7):
     if c == 5:
         break
-    dels = list(factorint(i).keys())
-    m = max(dels) + min(dels) if dels else 0
+    dels = divisors(i)[1:-1]
+    m = max(dels) + min(dels) if len(dels) >= 2 else 0
     if str(m)[-1] == "4":
         print(i, m)
         c += 1
 
-# 800007 284
+# 800004 400004
+# 800009 114294
 # 800013 266674
-# 800019 8084
-# 800035 3024
-# 800039 7584
+# 800024 400014
+# 800033 61554
